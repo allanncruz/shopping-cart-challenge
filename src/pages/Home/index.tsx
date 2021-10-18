24c75bc7827b0a5d03c26dd0389f461f14156219
allanncruz <contato@allancruz.com.br>
2021-10-18 16:59:57 -0300

    feat: set home page and routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ProductList, Container } from './styles';
+
+const Home = (): JSX.Element => {
+  return (
+    <Container>
+        <ProductList>
+     
+    </ProductList>
+    </Container>
+  );
+};
+
+export default Home;
